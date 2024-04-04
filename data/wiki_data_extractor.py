d31b6cbc56dee2948bbc4a08930ad4a598bcf9c5
Debshishu Ghosh <debshishug@kitaab.biz>
2024-04-04 11:30:40 +0530

    Basic usage of Spacy explained along with some NER

---
+++ data/wiki_data_extractor.py
@@ -0,0 +1,12 @@
+import bs4
+import sys
+import requests
+
+wiki_page = 'United States'
+res = requests.get(f'https://en.wikipedia.org/wiki/{wiki_page}' )
+res.raise_for_status()
+wiki = bs4.BeautifulSoup(res.text,"html.parser")
+
+with open(wiki_page+".txt", "w", encoding="utf-8") as f:
+    for i in wiki.select('p'):
+        f.write(i.getText())
